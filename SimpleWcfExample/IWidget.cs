cd26d1f6b8dd5555418530b899d79aaa8c0cc61e
bWalshPdx <bwalshpdx@gmail.com>
2016-06-08 15:06:33 -0700

    Simple name refactor of the default wcf solution.

---
+++ SimpleWcfExample/IWidget.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SimpleWcfExample
+{
+    
+    [ServiceContract]
+    public interface IWidget
+    {
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        ToyObject GetDataUsingDataContract(ToyObject composite);
+
+        // TODO: Add your service operations here
+    }
+
+    
+    [DataContract]
+    public class ToyObject
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+
+
+
+
+}
